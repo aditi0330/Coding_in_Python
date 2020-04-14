cf5e6edbdfa84a0b5a459786e69cb9d63508a605
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-04-14 15:20:31 +0530

    Add files via upload

---
+++ Datetime_date.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 19:38:27 2020
+
+@author: Lenovo
+"""
+
+#!python3
+
+from datetime import datetime
+from datetime import date
+
+datetime.today()
+#datetime.datetime(2020, 3, 19, 14, 38, 52, 133483)
+
+today = datetime.today()
+
+
+type(today)
+#<class 'datetime.datetime'>
+
+
+todaydate = date.today()
+
+todaydate
+#datetime.date(2020, 3, 19)
+
+type(todaydate)
+#<class 'datetime.date'>
+
+todaydate.month
+#2
+
+todaydate.year
+#2020
+
+todaydate.day
+#19
+
+
+christmas = date(2020, 12, 25)
+christmas
+#datetime.date(2020, 12, 25)
+
+if christmas is not todaydate:
+    print("Sorry there are still " + str((christmas - todaydate).days) + " until Christmas!")
+else:
+    print("Yay it's Christmas!")
+    
