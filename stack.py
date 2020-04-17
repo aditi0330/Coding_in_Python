6e4e665de00893f8dad983a415d8ba798f88b5ef
Aditi Shukla <42002395+aditi0330@users.noreply.github.com>
2020-04-18 00:11:49 +0530

    Stack using list and deque

---
+++ stack.py
@@ -0,0 +1,50 @@
+# 1. Python program to demonstrate stack implementation using list 
+
+stack = [] 
+
+# append() function to push element in the stack 
+
+stack.append('a') 
+stack.append('b') 
+stack.append('c') 
+
+print('Initial stack') 
+print(stack) 
+
+# pop() fucntion to pop element from stack in LIFO order 
+print('\nElements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+
+print('\nStack after elements are poped:') 
+print(stack) 
+
+
+
+# 2. Python program to demonstrate stack implementation using collections.deque 
+
+from collections import deque 
+
+stack = deque() 
+
+# append() function to push element in the stack 
+stack.append('a') 
+stack.append('b') 
+stack.append('c') 
+
+print('Initial stack:') 
+print(stack) 
+
+# pop() fucntion to pop element from stack in LIFO order 
+
+print('\nElements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+
+print('\nStack after elements are poped:') 
+print(stack) 
+
+
+
